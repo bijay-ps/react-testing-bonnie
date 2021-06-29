4c63e96dbd26140a2db02d03d48eb480f67f28b7
Bijay Singh <bijay.singh.ext@zeiss.com>
2021-06-29 12:06:47 +0530

    set up guessedWords test

---
+++ src/integration.test.js
@@ -0,0 +1,13 @@
+import { storeFactory } from "../test/testUtils";
+import { guessWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    it("should update the state correctly for unsuccessful guess ", () => {});
+    it("should update the state correctly for successful guess ", () => {});
+  });
+  describe("some guessed words", () => {
+    it("should update the state correctly for unsuccessful guess ", () => {});
+    it("should update the state correctly for successful guess ", () => {});
+  });
+});
