94b47c1b20fd186fe68cf83c5e2545113ad43a3c
Bijay Singh <bijay.singh.ext@zeiss.com>
2021-07-03 13:55:03 +0530

    changes useState to useReducer in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import "./App.css";
 
 import Congrats from "./Congrats";
@@ -6,13 +6,25 @@ import GuessedWords from "./GuessedWords";
 import Input from "./Input";
 import { getSecretWord } from "./actions";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "setSecretWord":
+      return { ...state, secretWord: action.payload };
+    default:
+      return throw new Error(`Invalid action type ${action.type}`);
+  }
+};
+
 function App() {
-  const [secretWord, setSecretWord] = useState("");
+  const [state, dispatch] = useReducer(reducer, { secretWord: "" });
 
   // TODO: get props from shared state
   const success = false;
   const guessedWords = [];
 
+  const setSecretWord = (secretWord) => {
+    dispatch({ type: "setSecretWord", payload: secretWord });
+  };
   useEffect(() => {
     getSecretWord(setSecretWord);
   }, []);
@@ -21,7 +33,7 @@ function App() {
     <div data-test="component-app" className="container">
       <h1>Jotto</h1>
       <Congrats success={success} />
-      <Input success={success} secretWord={secretWord} />
+      <Input success={success} secretWord={state.secretWord} />
       <GuessedWords guessedWords={guessedWords} />
     </div>
   );
